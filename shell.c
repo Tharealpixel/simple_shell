e2ea12b9d6a92ed3ac5fe3520ed47a4901601b7b
Tharealpixel <medboutahor@gmail.com>
2023-05-22 09:02:04 -0700

    shell 0.2 handles arguments

---
+++ shell.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+int main()
+{
+        char *buff = NULL;
+        size_t buff_size = 0;
+        char *token, **arr;
+        int status, i = 0;
+        pid_t pid;
+
+        while (1)
+        {
+                write(1, "#cisfun$ ", 9);
+		getline(&buff, &buff_size, stdin);
+                arr = malloc(sizeof(char *) * 1024);
+                token = strtok(buff, " \n\t");
+                while (token)
+                {
+                        arr[i] = token;
+                        token = strtok(NULL, " \n\t");
+                        i++;
+                }
+                arr[i] = NULL;
+                pid = fork();
+                if (pid == 0)
+                {
+                        if (execve(arr[0], arr, NULL) == -1)
+			{
+                                perror("exceve");
+				exit(0);
+			}
+                }
+                else
+                        wait(&status);
+
+                i = 0;
+                free(arr);
+        }
+	return (0);
+}
