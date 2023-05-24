2d62e6d1765bfb5683777503a62b45d30c7015ca
SAIYDY EL MEHDI <saiydymed94@gmail.com>
2023-05-24 07:06:56 -0700

    ALX SE

--- beatiful.c
+++ beatiful.c
@@ -1,63 +1,69 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024
+/**
+ * inter -Return if shell is interactive mode
+ * @inf: struct address
+ *
+ * Return: 1 if interactive mode
+ */
+int inter(inf_t *inf)
+{
+	return (isatty(STDIN_FILENO) && inf->readfd <= 2);
+}
 
 /**
- * main -Simple shell program
- *
- * Return: Always 0
+ * he_delimiter -checks function
+ * @c: the character
+ * @del: the string
+ * Return: 1 if true, 0 if false
  */
-int main(void)
+int he_delimiter(char c, char *del)
 {
-	char *buff;
-	char *token;
-	char *arg[10];
-	int j;
-
-	buff = malloc(sizeof(char) * BUFFER_SIZE);
-	if (buff == NULL)
-	{
-		perror("malloc error");
-		exit(EXIT_FAILUR);
-	}
-
-	while (1)
-	{
-		printf("$ ");
-		fgets(buffer, BUFFER_SIZE, stdin);
-
-		token = strtok(buffer, " \n");
-		j = 0;
-		while (token != NULL)
-		{
-			arg[j++] = token;
-			token = strtok(Null, "\n");
-		}
-		arg[i] = NULL;
-
-		if (strcmp(arg[0], "exit") == 0)
-			break;
-		pid_t pid = fork();
-
-		if (pid < 0)
-		{
-			perror("fork error");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execvp(arg[0], arg) == -1)
-			{
-				perror("execvp error");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-
-	free(buff);
+	while (*del)
+		if (*del++ == c)
+			return (1);
 	return (0);
+}
+
+/**
+ * _alpha -alpha checks alphabitic functions charcter
+ * @c: The character
+ * Return: 1 if c is alpha, 0 otherwise
+ */
+
+int _alpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _str -converts a string
+ * @s: string
+ * Return: 0 if no number in string
+ */
+int _str(char *s)
+{
+	int j, sign = 1, flag = 0, output;
+	unsigned int res = 0;
+
+	for (j = 0; s[j] != '\0' && flag != 2; j++)
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			flag = 1;
+			res *= 10;
+			res += (s[j] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+	if (sign == -1)
+		output = -res;
+	else
+		output = res;
+
+	return (output);
 }
